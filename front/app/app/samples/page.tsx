e8f13fbd651dc4badae4c658ec8e7651fdd3a92b
tkm-mkzk <tkm.mkzk.0604@gmail.com>
2023-09-25 09:43:49 +0900

    :sparkles: corsの設定とAPIテスト用sample作成

---
+++ front/app/app/samples/page.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+export default function Samples() {
+  const fetchSamples = async () => {
+    try {
+      const res = await fetch('http://localhost:3000/samples')
+      console.log(res)
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleButtonClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault()
+    void fetchSamples()
+  }
+
+  return (
+    <>
+      <form>
+        <button onClick={handleButtonClick}>sample</button>
+      </form>
+    </>
+  )
+}
